180ed27ea396c1f0964194d77d764f146c08a579
Matan Ohayon <matan1ohayon@gmail.com>
2026-01-11 14:38:27 +0200

    adding a messege in the order label

--- src/OrderLabel.jsx
+++ src/OrderLabel.jsx
@@ -4,7 +4,12 @@ function OrderLabel({ isVisible, isScrolled }) {
   
   return (
     <div className={`top-nav ${isVisible ? 'visible' : ''}`}>
-      <a href="http://wa.me/972525565860" target='_blank' className={`phone-btn ${isScrolled ? 'scrolled' : ''}`}>
+      <a 
+        href={`https://wa.me/972525565860?text=${encodeURIComponent('היי יותם! אשמח לבצע הזמנה של פופיז! 🍪🐈‍⬛')}`} 
+        target='_blank' 
+        className={`phone-btn ${isScrolled ? 'scrolled' : ''}`}
+        rel="noopener noreferrer"
+      >
         <h3>להזמנות</h3>
         <img src="WhatsApp_icon_w.png" alt="WhatsApp icon" />
       </a>
